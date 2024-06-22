9572d508f231c1bf9aa8bf10b39b3fb012ac76b1
NaweenMAGGAN <naween.maggan@epitech.eu>
2024-06-22 14:24:35 +0200

    [UPDATE] First Version2.0 - Epitech Edusign

--- index.js
+++ index.js
@@ -125,8 +125,18 @@ authorize().then(auth => {
 
 
 
+// Express part of the code
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 
 

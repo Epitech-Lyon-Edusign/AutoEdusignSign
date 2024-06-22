dda5050412b2f15cf86fea85bead4cf89a8ee4a1
NaweenMAGGAN <naween.maggan@epitech.eu>
2024-06-22 13:51:51 +0200

    [UPDATE] First Version2.0 - Epitech Edusign

--- app.js
+++ app.js
@@ -13,4 +13,5 @@ app.get('/', (req, res) => {
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Deployement Successfull`);
 });
